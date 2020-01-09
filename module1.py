6fd859608e76a66458351683dc97ca34b7ddd040
KritCMD <59676881+KritCMD@users.noreply.github.com>
2020-01-09 10:53:48 +0700

    Update module1.py

--- module1.py
+++ module1.py
@@ -91,5 +91,6 @@ def nose_feat_get(frame,nw,nh):
         return None
     nose_feat=('Nose',nosewperh,noselength)
     return nose_feat
+
 class all_feat:
     def __init__(self,eye_feat,face_feat,mouth_feat,nose_feat)
